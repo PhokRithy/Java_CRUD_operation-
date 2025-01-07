0fcb57c8c77e41f1de8f1020562128d68ccc6bc6
PhokRithy <158687728+PhokRithy@users.noreply.github.com>
2025-01-07 21:46:55 +0700

    first commit

---
+++ src/test/java/com/example/firststartspringboot/FirstStartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firststartspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStartSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
